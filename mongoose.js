38520c43780bd9ddcdbf2e4d2d7c64f3a096e2f6
Maharanalalatendu <lalatendumaharana0@gmail.com>
2024-11-05 14:40:15 +0530

    mongoose 1

--- mongoose.js
+++ mongoose.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 //require('dotenv').config();
 //const mongoURI = process.env.MONGODB_URI;
 ///////////
-     mongoose.connect("mongodb+srv://lalatendumaharana0:lala1234@cluster0.vnbsj.mongodb.net/")
-
+    // mongoose.connect("mongodb+srv://lalatendumaharana0:lala1234@cluster0.vnbsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb+srv://lalatendumaharana0:lalatendu933@cluster0.vnbsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 // Define the user schema
 const userSchema =mongoose.Schema({
   user_name: String,
